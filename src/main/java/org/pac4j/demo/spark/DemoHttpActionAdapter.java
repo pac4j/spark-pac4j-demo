6d35a14fa79afa6ed254a3b83f8ee293350b013b
leleuj <leleuj@gmail.com>
2017-09-06 09:14:40 +0200

    upgrade to pac4j v2.1 and Spark 2.6

--- src/main/java/org/pac4j/demo/spark/DemoHttpActionAdapter.java
+++ src/main/java/org/pac4j/demo/spark/DemoHttpActionAdapter.java
@@ -4,11 +4,10 @@ import org.pac4j.core.context.HttpConstants;
 import org.pac4j.sparkjava.DefaultHttpActionAdapter;
 import org.pac4j.sparkjava.SparkWebContext;
 import spark.ModelAndView;
+import spark.Spark;
 import spark.TemplateEngine;
 
 import java.util.HashMap;
-
-import static spark.Spark.halt;
 
 public class DemoHttpActionAdapter extends DefaultHttpActionAdapter {
 
@@ -21,9 +20,9 @@ public class DemoHttpActionAdapter extends DefaultHttpActionAdapter {
     @Override
     public Object adapt(int code, SparkWebContext context) {
         if (code == HttpConstants.UNAUTHORIZED) {
-            halt(401, templateEngine.render(new ModelAndView(new HashMap<>(), "error401.mustache")));
+            Spark.halt(401, templateEngine.render(new ModelAndView(new HashMap<>(), "error401.mustache")));
         } else if (code == HttpConstants.FORBIDDEN) {
-            halt(403, templateEngine.render(new ModelAndView(new HashMap<>(), "error403.mustache")));
+            Spark.halt(403, templateEngine.render(new ModelAndView(new HashMap<>(), "error403.mustache")));
         } else {
             return super.adapt(code, context);
         }
