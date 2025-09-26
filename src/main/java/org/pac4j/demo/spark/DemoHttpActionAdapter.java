69058cb8111149676b10fe368b72348a88c4eb9d
Jérôme LELEU <leleuj@gmail.com>
2025-09-26 11:24:40 +0200

    fix savedAuthHeader

--- src/main/java/org/pac4j/demo/spark/DemoHttpActionAdapter.java
+++ src/main/java/org/pac4j/demo/spark/DemoHttpActionAdapter.java
@@ -1,9 +1,11 @@
 package org.pac4j.demo.spark;
 
+import lombok.val;
 import org.pac4j.core.context.HttpConstants;
 import org.pac4j.core.context.WebContext;
 import org.pac4j.core.exception.TechnicalException;
 import org.pac4j.core.exception.http.HttpAction;
+import org.pac4j.jee.context.JEEContext;
 import org.pac4j.sparkjava.SparkHttpActionAdapter;
 import spark.ModelAndView;
 import spark.TemplateEngine;
@@ -23,6 +25,11 @@ public class DemoHttpActionAdapter extends SparkHttpActionAdapter {
         if (action != null) {
             final int code = action.getCode();
             if (code == HttpConstants.UNAUTHORIZED) {
+                val jeeContext = (JEEContext) context;
+                val savedAuthHeader = jeeContext.getSavedAuthenticateHeader();
+                if (savedAuthHeader != null) {
+                    jeeContext.getNativeResponse().setHeader(HttpConstants.AUTHENTICATE_HEADER, savedAuthHeader);
+                }
                 stop(401, templateEngine.render(new ModelAndView(new HashMap<>(), "error401.mustache")));
             } else if (code == HttpConstants.FORBIDDEN) {
                 stop(403, templateEngine.render(new ModelAndView(new HashMap<>(), "error403.mustache")));
